f43e54e07641b06336fa8fe34c9eca8ba98e03ff
ray-seal <rayseal1986@gmail.com>
2025-09-11 05:57:41 +0100

    Fix import statement for android.view.View

--- app/src/main/java/com/rayseal/supportapp/MainActivity.java
+++ app/src/main/java/com/rayseal/supportapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.rayseal.supportapp;
 
-import android.view.View
+import android.view.View;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
