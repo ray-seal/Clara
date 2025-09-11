4d376fc2e0b1d6eae50bb4c2ef42b112b74c8021
ray-seal <rayseal1986@gmail.com>
2025-09-11 00:03:51 +0000

    gradle added

---
+++ app/src/main/app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+id("com.android.application")
+id("com.google.gms.google-services")
+}
+
+android {
+compileSdk = 34
+defaultConfig {
+applicationId = "com.rayseal.supportapp"
+minSdk = 21
+targetSdk = 34
+versionCode = 1
+versionName = "1.0"
+}
+buildTypes {
+getByName("release") {
+isMinifyEnabled = false
+proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+}
+}
+}
+
+dependencies {
+implementation("com.google.firebase:firebase-auth:22.3.0")
+implementation("com.google.android.gms:play-services-auth:20.7.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+buildscript {
+dependencies {
+classpath("com.google.gms:google-sevices:4.4.1")
+}
+}
