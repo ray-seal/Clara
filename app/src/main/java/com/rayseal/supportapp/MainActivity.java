122803ddfa7c825a2b6d1ed634712411a5661cce
ray-seal <rayseal1986@gmail.com>
2025-09-11 00:36:21 +0000

    updates

--- app/src/main/java/com/rayseal/supportapp/MainActivity.java
+++ app/src/main/java/com/rayseal/supportapp/MainActivity.java
@@ -6,20 +6,46 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-    public class MainActivity extends AppCmpatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+iimportandroidx.annotation.NonNull;
+
+    public class MainActivity extends AppCompatActivity {
+        private FirebaseAuth mAuth;
+        private TextView welcomeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView welcomeText = findViewById(R.id.welcomeText);
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        welcomeText = findViewById(R.id.welcomeText);
         Button supportButton = findViewById(R.id.supportButton);
 
-        supportButton.setOnClickistener(new View.OnClickListener() {
+        supportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                welcomeText.setText("You are not alone. Support is available!");
+                // Try Anon Sign in
+                mAuth.signInAnonymously()
+                .addOnCompleteListener(MainActivity.this, new onCmpleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            welcomeText.setText("Signed in anonymously! You are not alone. Support is available!");
+            } else {
+                welcomeText.setText("Auth failed: " + task.getException().getMessage());
             }
-        });
+        }
+    });
+}
+});
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ mavenCentral()
 }
 dependencies {
 classpath("com.android.tools.build:gradle:8.0.2")
-classpath("com.google.gms:google-sevices:4.4.1")
+classpath("com.google.gms:google-services:4.4.1")
 }
 }
