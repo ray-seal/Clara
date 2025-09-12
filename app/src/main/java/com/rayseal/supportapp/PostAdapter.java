bbcaee7e5c15739336b388d68c4f680c3786c25a
ray-seal <rayseal1986@gmail.com>
2025-09-12 02:41:38 +0100

    Implement PostAdapter for RecyclerView

---
+++ app/src/main/java/com/rayseal/supportapp/PostAdapter.java
@@ -0,0 +1,46 @@
+package com.rayseal.supportapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+  private List<Post> posts;
+
+public PostAdapter(List<Post> posts) {
+  this.posts = posts;
+}
+
+@NonNull
+  @Override
+  public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    view v = LayoutInflater.from(parent.getContext())
+      .inflate(R.layout.item_post, parent, false);
+    return new PostViewHolder(v);
+  }
+
+@Override
+  public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+    Post post = posts.get(position);
+    holder.postContentText.setText(post.content);
+    holder.postCategoriesText.setText("Categories: " + String.join(", ", post.categories));
+  }
+
+@Override
+  public int getItemCount() {
+    return posts.size();
+  }
+
+static class PostViewHolder extends RecyclerView.VewHolder {
+  TextView postContentText, postCategoriesText;
+  PostViewHolder(View itemView) {
+    super(itemView);
+    postContentText = itemView.findViewById(R.id.postContentText);
+    postCategoriesText = itemView.findViewById(R.id.postCategoriesText);
+  }
+}
+}
