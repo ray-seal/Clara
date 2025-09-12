d0a3a0af89e1ccfc4aeac382ecdf4369cd1b9a55
ray-seal <rayseal1986@gmail.com>
2025-09-12 04:59:15 +0100

    Add import for Color class in PublicFeedActivity

--- app/src/main/java/com/rayseal/supportapp/PublicFeedActivity.java
+++ app/src/main/java/com/rayseal/supportapp/PublicFeedActivity.java
@@ -3,6 +3,7 @@ package com.rayseal.supportapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import android.graphics.Color;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
