0459f5e89c024e13f1c0c35fc907ff80a4625bb0
ray-seal <rayseal1986@gmail.com>
2025-09-12 02:29:06 +0100

    Create Post class with content and categories

---
+++ app/src/main/java/com/rayseal/supportapp/Post.java
@@ -0,0 +1,12 @@
+package com.rayseal.supportapp;
+
+import java.util.list;
+
+public class Post {
+  public String content;
+  public List<String> categories;
+  public Post(String content, List<String> categories) {
+    this.content = content;
+    this.categories = categories;
+  }
+}
