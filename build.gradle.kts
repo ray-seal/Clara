d07bb01db0284f84bcf4d4b651372390b3de6e53
rayseal <rayseal1986@gmail.com>
2025-09-20 05:42:25 +0100

    Fix Syntax errors and removed useless build gradle

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-repositories {
-google()
-mavenCentral()
-}
-dependencies {
-classpath("com.android.tools.build:gradle:8.4.0")
-classpath("com.google.gms:google-services:4.4.1")
-}
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,18 @@
 pluginManagement {
-repositories {
-google()
-mavenCentral()
-gradlePluginPortal()
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
 }
 plugins {
     id("com.android.application") version "8.4.0"
-    id(com.google.gms.google-services") version "4.4.1"
+    id("com.google.gms.google-services") version "4.4.1"
 }
 }
 dependencyResolutionManagement {
-repositories {
-google()
-mavenCentral()
+    repositories {
+        google()
+        mavenCentral()
 }
 }
 rootProject.name = "Clara"
